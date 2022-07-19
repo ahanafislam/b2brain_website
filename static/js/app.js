49f42b0abe16e20c7782cbc6281c08cc3e5d39c4
ahanafislam <ahanafislam@gmail.com>
2022-07-19 13:00:26 +0600

    Create basic search event

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,31 @@
 const toggleIcon = (event) => {
     event.classList.toggle("dropdown-open")
 }
+
+// Function for search input
+const searchInput = document.querySelector('#search-input');
+const searchIcon = document.querySelector('#search-icon');
+const searchIconWrapper = document.querySelector('#search-icon-wrapper')
+const contentArea = document.querySelector("#content-area");
+
+// Function for Close search
+const close = () => {
+    const closeBtn = document.querySelector('#close-btn');
+    closeBtn.remove();
+    contentArea.style.display = "block";
+    searchIcon.style.display = 'inline';
+}
+
+// Listen event for focus search input
+searchInput.addEventListener('focus', e => {
+    const span = document.createElement('span');
+    searchIcon.style.display = 'none';
+    span.innerHTML = `<img class='mx-1' src="static/img/close.png" alt="close" onclick='close()'/>`;
+    span.setAttribute('id', 'close-btn');
+    searchIconWrapper.appendChild(span);
+    contentArea.style.display = "none";
+});
+
+// Listen event for blur search input
+searchInput.addEventListener('blur', close);
+
